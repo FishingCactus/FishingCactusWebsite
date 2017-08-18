2ffaa3705491c76ee89274845620862c298b6077
Pieter Vantorre <pieter.vantorre@fishingcactus.com>
2017-08-18 10:36:54 +0200

    Simplified and fixed cactus appearing effect

--- static/js/custom.js
+++ static/js/custom.js
@@ -5,10 +5,10 @@ var cacti = [
     "../img/cactus_clark.svg",
     ];
 var cacti_images = [
-    {img: null, used:false},
-    {img: null, used:false},
-    {img: null, used:false},
-    {img: null, used:false}
+    null,
+    null,
+    null,
+    null
     ];
 
 $(document).ready(function(){
@@ -18,26 +18,26 @@ $(document).ready(function(){
         var img = $(img);
         img.hide();
         img.addClass("cactus img-responsive");
-        cacti_images[i].img = img;
+        cacti_images[i] = img;
     }
     $("nav .navbar-collapse li").filter(function() {
         return !$(this).hasClass("hidden");
     }).each(function(index) {
         $(this).hover(
             function(){
-                if ( index <= cacti.length && !cacti_images[index].used ) {
-                    cacti_images[index].used = true;
-                    var img = cacti_images[index].img;
+                if ( index <= cacti.length ) {
+                    var img = cacti_images[index];
                     img.removeAttr("style");
                     img.hide();
                     img.appendTo("body");
                     var position = $(this).position();
                     img.css({ "left": position.left + ($(this).width() - img.width()) / 2.0});
-                    img.slideDown(200);
+                    img.stop(false,true);
+                    img.slideDown({ "queue": true, "speed": 200});
                 }
             },
             function() {
-                cacti_images[index].img.slideUp({ "queue": false, "complete": function() {cacti_images[index].used = false; $(this).remove();}, "speed": 200 });
+                cacti_images[index].slideUp({ "queue": true, "speed": 200 });
             }
         );
     });
